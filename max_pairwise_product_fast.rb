0aab0f997e3d1e1b1fb780fc2cd87311d51b06a7
akatriel <michael.c.gabay@gmail.com>
2016-06-19 14:46:23 -0400

    added fast solution for max pairwise product

--- max_pairwise_product_fast.rb
+++ max_pairwise_product_fast.rb
@@ -2,7 +2,7 @@
 
 # Given a sequence of non-negative integers a0,…,an−1, find the maximum pairwise product, that is, the largest integer that can be obtained by multiplying two different elements from the sequence (or, more formally, max0≤i≠j≤n−1aiaj). Different elements here mean ai and aj with i≠j (it can be the case that ai=aj).
 # p 'enter first number'
-
+require 'benchmark'
 max_number = gets.chomp.to_i
 
 # p 'enter numbers separated by spaces'
@@ -16,21 +16,25 @@ stringValues.map{|v| intValues.push(Integer(v, 10)) }
 
 
 def maxPairwise nums
+
 	n = nums.length
-	result = 0
+
+	max_index1 = -1
 	for i in (0...n)
-		j = 0
-		loop do
-			j += 1
-			break if j == n
-			product = nums[i] * nums[j]
-			if product > result && nums[i] != nums[j]
-				result = product
-			end
-			
+		if max_index1 == -1 || nums[i] > nums[max_index1]
+			max_index1 = i
 		end
 	end
-	p result
+
+	max_index2 = -1
+	for j in (0...n)
+		if nums[j]!= nums[max_index1] && (max_index2 == -1 || nums[j] > nums[max_index2])
+			max_index2 = j
+		end
+	end
+
+	# p nums[max_index2] * nums[max_index1]
+	p Benchmark.measure{nums[max_index2] * nums[max_index1]}
 end
 
-maxPairwise intValues
+maxPairwise intValues	
