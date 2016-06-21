37520d668055aff8213195c443b010eb614ebaee
akatriel <michael.c.gabay@gmail.com>
2016-06-21 11:34:48 -0400

    fixed gcd.

--- Week2/greatest_common_divisor.rb
+++ Week2/greatest_common_divisor.rb
@@ -2,8 +2,7 @@ def euclidGCD a, b #Euclid Algorithm for finding Greatest Common Divisor
 	if b == 0
 		a
 	else
-		aPrime = a / b
-		euclidGCD b, aPrime
+		euclidGCD b, a % b	
 	end	
 end
-p euclidGCD 3918848, 1653264
+p euclidGCD 1071, 462
